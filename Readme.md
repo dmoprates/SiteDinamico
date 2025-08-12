9c46d3e13b70f0690ffce78bec78d5647996328e
dmoprates <prates.diego@gmail.com>
2025-08-12 16:03:38 -0300

    chore: Marcação inicial do index, inclusão de imagem placeholder e início do Readme

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Projeto Site Dinâmico
+
+- Sistema criado no Curso "Desenvolvimento Web Completo - Danki Code"
+- Linguagens de programação estudadas:
+    - PHP;
+    - HTML;
+    - CSS;
+    - Banco de Dados
+
+Créditos para os templates à Danki Code
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Dinâmico</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo">Logomarca</div><!--logo-->
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--desktop-->
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--mobile-->
+    </header>
+    <section class="banner-principal">
+        <form action="">
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required>
+            <input type="submit" name="acao" value="Cadastrar!">
+        </form>
+    </section><!--banner-principal-->
+    <section class="descricao-autor">
+        <div class="w50">
+            <h2>Lorem Ipsum Dolor</h2>
+            <p>Sit occaecat dolore sunt dolore occaecat aute aliqua Lorem. Fugiat ut dolore ex eu. Ea do excepteur
+                labore pariatur consequat reprehenderit proident id ex. Elit duis ut est in labore. Do duis enim ut
+                aliquip
+                adipisicing fugiat. Sit qui qui mollit et et deserunt aliqua sunt. Occaecat veniam labore aliqua
+                adipisicing consequat anim ea laborum amet duis. Lorem irure magna ad incididunt pariatur duis in minim
+                enim veniam
+                incididunt aliquip. Ex fugiat magna culpa sunt incididunt consectetur pariatur laboris amet. Magna ex
+                ullamco minim ut dolore amet culpa Lorem voluptate sint. Mollit amet est ex deserunt magna est deserunt
+                exercitation deserunt.</p>
+            <p>Velit laboris irure id eiusmod labore commodo. Velit voluptate exercitation excepteur in cupidatat elit
+                incididunt ad enim magna. Velit nostrud enim nisi eiusmod laboris. Exercitation excepteur adipisicing
+                eiusmod cupidatat enim occaecat quis. Anim voluptate duis proident aliquip eiusmod sit nostrud.</p>
+        </div><!--w50-->
+        <div class="w50">
+            <img src="images/foto.jpg" alt="User image">
+        </div>
+    </section><!--descricao-autor-->
+</body>
+
+</html>
