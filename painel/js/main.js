e0e9b000d1b6a03e695c028c06332962cc6fef2c
dmoprates <prates.diego@gmail.com>
2025-08-20 14:32:33 -0300

    feat: funcionalidade de paginação

--- painel/js/main.js
+++ painel/js/main.js
@@ -1,42 +1,55 @@
-$(function(){
+$(function () {
     var open = true;
     var windowSize = $(window)[0].innerWidth;
-    var targetSizeMenu = (windowSize <= 400) ? 200: 300;
-    if(windowSize <= 768){
-        $('.menu').css('width','0').css('padding','0');
+
+    var targetSizeMenu = (windowSize <= 400) ? 200 : 250;
+
+    if (windowSize <= 768) {
+        $('.menu').css('width', '0').css('padding', '0');
         open = false;
     }
-    $('.menu-btn').click(function(){
-        if(open){
-            $('.menu').animate({'width': '0', 'padding':'0'}, function(){
+
+    $('.menu-btn').click(function () {
+        if (open) {
+            //O menu está aberto, precisamos fechar e adaptar nosso conteudo geral do painel
+            $('.menu').animate({ 'width': 0, 'padding': 0 }, function () {
                 open = false;
             });
-            $('.content, header').css('width', '100%');
-            $('.content, header').animate({'left':'0'}, function(){
+            $('.content,header').css('width', '100%');
+            $('.content,header').animate({ 'left': 0 }, function () {
                 open = false;
             });
-        }else{
+        } else {
+            //O menu está fechado
             $('.menu').css('display', 'block');
-            $('.menu').animate({'width': targetSizeMenu+'px', 'padding':'10px'}, function(){
+            $('.menu').animate({ 'width': targetSizeMenu + 'px', 'padding': '10px 0' }, function () {
                 open = true;
             });
-            //$('.content, header').css('width', 'calc(100% - 300px)');
-            $('.content, header').animate({'left':targetSizeMenu+'px'}, function(){
+            if (windowSize > 768)
+                $('.content,header').css('width', 'calc(100% - 250px)');
+            $('.content,header').animate({ 'left': targetSizeMenu + 'px' }, function () {
                 open = true;
             });
         }
     })
-    $(window).resize(function(){
+    $(window).resize(function () {
         windowSize = $(window)[0].innerWidth;
-        if(windowSize <= 768){
+        targetSizeMenu = (windowSize <= 400) ? 200 : 250;
+        if (windowSize <= 768) {
             $('.menu').css('width', '0').css('padding', '0');
-            $('.content, header').css('width', '100%').css('left', '0');
+            $('.content,header').css('width', '100%').css('left', '0');
             open = false;
         } else {
-            open = true;
-            $('.content, header').css('width', 'calc(100% - 250px)').css('left', '250px');
-            $('.menu').css('width', '250px').css('padding', '10px 0');
+            $('.menu').animate({ 'width': targetSizeMenu + 'px', 'padding': '10px 0' }, function () {
+                open = true;
+            });
+
+            $('.content,header').css('width', 'calc(100% - 250px)');
+            $('.content,header').animate({ 'left': targetSizeMenu + 'px' }, function () {
+                open = true;
+            });
         }
+
     })
     $('[formato=data]').mask('99/99/9999');
 })
--- painel/pages/listar-depoimentos.php
+++ painel/pages/listar-depoimentos.php
@@ -1,27 +1,43 @@
 <?php
-    $depoimentos = Painel::selectAll('site.depoimentos');
+    $paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+    $porPagina = 2;
+        $depoimentos = Painel::selectAll('site.depoimentos', ($paginaAtual -1) * $porPagina, $porPagina*$paginaAtual);
+
 ?>
 
 <div class="box-content">
     <h2> <i class="fa-solid fa-message"></i> Depoimentos Cadastrados</h2>
 
-    <table>
-        <tr>
-            <td><i class="fa-solid fa-signature"></i> Nome</td>
-            <td><i class="fa-solid fa-comments"></i> Depoimento</td>
-            <td><i class="fa-solid fa-calendar-days"></i> Data</td>
-            <td><i class="fa-solid fa-pen-to-square"></i> Editar</td>
-            <td><i class="fa-solid fa-trash"></i> Deletar</td>
-        </tr>
-        <?php foreach ($depoimentos as $key => $value){?>
-        <tr>
-            <td><?php echo $value['nome'] ?></td>
-            <td><?php echo $value['depoimento'] ?></td>
-            <td><?php echo $value['data'] ?></td>
-            <td><a class="btn edit" href=""><i class="fa-solid fa-pen-to-square"></i></a></td>
-            <td><a class="btn delete" href=""><i class="fa-solid fa-trash"></i></a></td>
-        </tr>
-        <?php } ?>
-    </table>
+    <div class="wraper-table">
+        <table>
+            <tr>
+                <td><i class="fa-solid fa-signature"></i> Nome</td>
+                <td><i class="fa-solid fa-comments"></i> Depoimento</td>
+                <td><i class="fa-solid fa-calendar-days"></i> Data</td>
+                <td><i class="fa-solid fa-pen-to-square"></i> Editar</td>
+                <td><i class="fa-solid fa-trash"></i> Deletar</td>
+            </tr>
+            <?php foreach ($depoimentos as $key => $value) { ?>
+                <tr>
+                    <td><?php echo $value['nome'] ?></td>
+                    <td><?php echo $value['depoimento'] ?></td>
+                    <td><?php echo $value['data'] ?></td>
+                    <td><a class="btn edit" href=""><i class="fa-solid fa-pen-to-square"></i></a></td>
+                    <td><a class="btn delete" href=""><i class="fa-solid fa-trash"></i></a></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div><!--wraper-table-->
+    <div class="paginacao">
+        <?php
+            $totalPaginas = ceil(count(Painel::selectAll('site.depoimentos')) / $porPagina);
+            for($i = 1; $i <= $totalPaginas; $i++){
+                if($i == $paginaAtual)
+                    echo '<a class="page-selected" href="'.INCLUDE_PATH_PAINEL.'listar-depoimentos?pagina='.$i.'">'.$i.'</a>';
+                else
+                    echo '<a href="'.INCLUDE_PATH_PAINEL.'listar-depoimentos?pagina='.$i.'">'.$i.'</a>';
+            }
+        ?>
+    </div><!--paginacao-->
 
 </div><!--box-content-->
